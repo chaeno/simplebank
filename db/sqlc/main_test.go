4961cd2965c4507b73a693e9f0465b7854289c54
정노현 <53748656+chaeno@users.noreply.github.com>
2024-11-24 23:55:50 +0900

    fin session 2

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,19 @@ import (
 	"os"
 	"testing"
 
+	"github.com/chaeno/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver  = "postgres"
-	dbSourece = "postgres://root:secret@localhost:9400/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-
-	testDB, err = sql.Open(dbDriver, dbSourece)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/chaeno/simplebank/api"
+	db "github.com/chaeno/simplebank/db/sqlc"
+	"github.com/chaeno/simplebank/util"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
